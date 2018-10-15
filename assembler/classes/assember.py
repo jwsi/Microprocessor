3b7a3450cec637df0577ba8e7f48b637b6768dd3
James Webb <jw15520@bristol.ac.uk>
2018-10-15 18:35:15 +0100

    Basic assembler input/output achieved.
    Help messages defined for assembler.

--- assembler/classes/assember.py
+++ assembler/classes/assember.py
@@ -2,9 +2,33 @@ class Assembler():
     """
     This class is the main assembler class defining all operations.
     """
+    # Define input and output file
     input_file = None
     output_file = None
 
+    # Define memory dictionary which will be dumped into machine code.
+    memory = dict()
+
     def __init__(self, input_file, output_file):
-        input_file = input_file
-        output_file = output_file
+        """
+        Constructor for the Assembler class.
+        :param input_file: input source assembly file.
+        :param output_file: output file to write to (or stdout if None)
+        """
+        self.input_file = input_file
+        self.output_file = output_file
+
+
+    def output(self):
+        """
+        Outputs calculated machine code to the requested destination.
+        """
+        if self.output_file is None:
+            print("assembly")
+        else:
+            f = open(self.output_file, 'w')
+            f.write("assembly")
+            f.close()
+
+
+
--- assembler/main.py
+++ assembler/main.py
@@ -11,6 +11,7 @@ def main(args):
     :return: Machine code written to output or stdout if None specified.
     """
     assembler = Assembler(args.file, args.output)
+    assembler.output()
 
 
 if __name__ == "__main__":
