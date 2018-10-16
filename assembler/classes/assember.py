16e015c94bfa1d8aab27a8299d64ec0813b2da9a
James Webb <jw15520@bristol.ac.uk>
2018-10-16 15:39:47 +0100

    Add comment & whitespace removal to assembler.
    Instructions and labels now parse correctly

--- assembler/classes/assember.py
+++ assembler/classes/assember.py
@@ -10,6 +10,7 @@ class Assembler():
     next_address = 0
     memory = dict()
     labels = dict()
+    instructions = []
 
     def __init__(self, input_file, output_file):
         """
@@ -53,20 +54,46 @@ class Assembler():
 
     def build_data(self, data_segment):
         for line in data_segment:
-            try:
-                label, operand = line.split(":")[0], line.split(":")[1].strip()
-            except:
-                # Skip invalid lines
-                continue
-            self.insert_data(label, operand)
+            if line.strip():
+                if line.strip()[0] == "#":
+                    # Ignore full line comments
+                    continue
+                label, operand = line.split(":")[0], line.split(":")[1].strip().split("#")[0]
+                print(label + " " + operand)
+                self.insert_data(label, operand)
 
+
+    def build_instructions(self, instruction_segment):
+        for line in instruction_segment:
+            if len(line.split(":")) > 1:
+                # Add label definition
+                label = line.split(":")[0].strip()
+                self.labels[label] = self.next_address
+            elif line.strip():
+                if line.strip()[0] == "#":
+                    # Ignore full line comments
+                    continue
+                operation, operand = line.strip().split(" ")[0], line.strip().split(" ", 1)[1]
+                instruction = []
+                instruction.append(operation)
+                for part in operand.split(","):
+                    instruction.append(part.strip())
+                self.instructions.append(instruction)
+                print(instruction)
 
 
     def first_pass(self):
+        print("variables")
         data_segment = self.assembly.split(".data")[1].split(".text")[0].split("\n")
         self.build_data(data_segment)
+        print()
         print("memory")
         print(self.memory)
+        print()
+        print("instructions")
+        instruction_segment = self.assembly.split(".text")[1].split("\n")
+        self.build_instructions(instruction_segment)
+        print()
         print("labels")
         print(self.labels)
 
