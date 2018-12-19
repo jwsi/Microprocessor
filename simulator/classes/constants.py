651e9f911ef8df517299aa3bbb49661fe3b3d13a
James Webb <james@ultra-horizon.com>
2018-12-19 10:52:23 +0000

    Add pi program

--- simulator/classes/constants.py
+++ simulator/classes/constants.py
@@ -1,3 +1,3 @@
-instruction_time = 0.5 # Time taken per instruction.
+instruction_time = 0.25 # Time taken per instruction.
 debug = False          # Define whether the program should be run in `debug` mode.
 N = 4                  # Define N to represent an n-way superscalar design.
